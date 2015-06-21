406bf467ab37e6a78172f50eb6192be7cc1e147f
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2015-06-21 03:10:09 +0200

    updated pluginsdk

--- pluginsdk/bridgemain.h
+++ pluginsdk/bridgemain.h
@@ -683,6 +683,7 @@ BRIDGE_IMPEXP bool DbgGetStringAt(duint addr, char* text);
 BRIDGE_IMPEXP const DBGFUNCTIONS* DbgFunctions();
 BRIDGE_IMPEXP bool DbgWinEvent(MSG* message, long* result);
 BRIDGE_IMPEXP bool DbgWinEventGlobal(MSG* message);
+BRIDGE_IMPEXP bool DbgIsRunning();
 
 //Gui defines
 #define GUI_PLUGIN_MENU 0
@@ -759,7 +760,8 @@ typedef enum
     GUI_REF_INITIALIZE,             // param1=const char* name,     param2=unused
     GUI_LOAD_SOURCE_FILE,           // param1=const char* path,     param2=line
     GUI_MENU_SET_ICON,              // param1=int hMenu,            param2=ICONINFO*
-    GUI_MENU_SET_ENTRY_ICON         // param1=int hEntry,           param2=ICONINFO*
+    GUI_MENU_SET_ENTRY_ICON,        // param1=int hEntry,           param2=ICONINFO*
+    GUI_SHOW_CPU                    // param1=unused,               param2=unused
 } GUIMSG;
 
 //GUI structures
@@ -845,6 +847,7 @@ BRIDGE_IMPEXP void GuiUpdateMemoryView();
 BRIDGE_IMPEXP void GuiLoadSourceFile(const char* path, int line);
 BRIDGE_IMPEXP void GuiMenuSetIcon(int hMenu, const ICONDATA* icon);
 BRIDGE_IMPEXP void GuiMenuSetEntryIcon(int hEntry, const ICONDATA* icon);
+BRIDGE_IMPEXP void GuiShowCpu();
 
 #ifdef __cplusplus
 }
